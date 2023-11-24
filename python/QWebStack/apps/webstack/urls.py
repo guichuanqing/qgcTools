440f836c2a6fa3314e675d4014d7d9ca02fc6207
guichuanqing <569011533@qq.com>
2023-11-24 11:16:38 +0800

    'addindex.html'

--- python/QWebStack/apps/webstack/urls.py
+++ python/QWebStack/apps/webstack/urls.py
@@ -4,6 +4,6 @@ from .views import index_view, upload_xmind
 
 urlpatterns = [
     re_path(r'^upload\.html$', upload_xmind, name='upload_xmind'),
-    path('', index_view, name='index'),  # 主页，自然排序
+    # path('', index_view, name='index'),  # 主页，自然排序
     re_path(r'^', index_view, name='index'),
 ]
